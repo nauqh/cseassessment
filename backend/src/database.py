56f4a7db389402716fa51ff432c8cc3e2992923c
Nauqh <hodominhquan.work@gmail.com>
2025-04-02 21:51:27 +1100

    update(submissions.py): set db connection for Neon

--- backend/src/database.py
+++ backend/src/database.py
@@ -10,7 +10,12 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.environ["DB_CONNECTION"]
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SETUP for NEON DB: https://neon.tech/docs/guides/sqlalchemy
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_recycle=1800,  # Recycle connections after 30 minutes
+    pool_pre_ping=True  # Check connection is alive before query execution
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
